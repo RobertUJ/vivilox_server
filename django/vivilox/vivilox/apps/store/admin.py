e1a8aacf1e6ef4a39c9c31fa9cb2333e714ccd2d
robertuj <robertuj@gmail.com>
2013-02-14 11:41:20 -0700

    Update Folder equeal server webfactional

---
+++ django/vivilox/vivilox/apps/store/admin.py
@@ -0,0 +1,12 @@
+from vivilox.apps.store.models import category, item, expedient_item, top_rated_cost,purchases,downloads 
+from django.contrib import admin
+
+admin.site.register(category)
+admin.site.register(item)
+admin.site.register(top_rated_cost)
+admin.site.register(expedient_item)
+admin.site.register(purchases)
+admin.site.register(downloads)
+
+
+
