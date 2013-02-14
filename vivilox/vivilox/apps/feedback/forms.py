74b1a42b03f470b7c2e57ef402ae9380c41f6a06
robertuj <robertuj@gmail.com>
2013-02-14 12:38:57 -0700

    delete folder vivilox again -- master

--- vivilox/vivilox/apps/feedback/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm 
-from django import forms
-from vivilox.apps.feedback.models import feedbacks
-
-
-class formFeedBack(forms.ModelForm):
-	class Meta:
-		model = feedbacks
-		exclude = ('date','client','artist','proposal','contest','sender',)
--- vivilox/vivilox/apps/tools/admin.py
+++
@@ -1,7 +0,0 @@
-#encoding:utf-8
-from django.contrib import admin
-from vivilox.apps.tools.models import country,province,tax
-admin.site.register(country)
-admin.site.register(province)
-admin.site.register(tax)
-# admin.site.register(sale_percentage)
